d4925724c4f304378dfc19a0cbf903b5330870a4
erindcole <erin.cole@teradata.com>
2016-09-12 15:49:11 -0700

    fixed and tested DECODE()

--- test/testDECODE.R
+++ test/testDECODE.R
@@ -1,3 +1,20 @@
+# Below is the table used for the test, called "tab1"
+#
+# c1  
+# ----
+# 5 
+# 4 
+# 3 
+# 2 
+
 tdf <- td.data.frame("test")
-tdf["c10"] <- DECODE(tdf["c1"], default=NULL, 1, 'Alpha', 2, 'Bravo', 3, 'Charlie', 4, 'Delta')
-as.td.data.frame(tdf, tableName="test47")
+tdf["c10"] <- DECODE(tdf["c1"], default='none', 1, 'Alpha', 2, 'Bravo', 3, 'Charlie', 4, 'Delta')
+as.td.data.frame(tdf, tableName="tab2")
+
+#this is the resulting table, called "tab2"
+# c1    c10
+# ---- -------
+# 5    none
+# 4    Delta
+# 3    Charlie
+# 2    Bravo
