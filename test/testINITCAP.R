9a20c67ef4254d19da872ca26a627f35d3e2a82d
erindcole <erin.cole@teradata.com>
2016-09-13 17:18:47 -0700

    made test cases more usable

--- test/testINITCAP.R
+++ test/testINITCAP.R
@@ -1,20 +1,33 @@
 # Below is the table used for the test, called "test"
-#         c1  c2        c3  c4  c5          c6
-#-----------  --------  --  --  ----------  ----------
-#  5  Emily         ?   cat         dog
-#  4  Daisy     ?   ?   fork        spoon
-#  3  Hank      ?   ?   ball        bat
-#  2  Amy       ?   ?   robot       human
+#        c1         c2          c3        
+# ---------- ----------- ----------
+#         3       ball          bat
+#         4       robot       human
+#         1         cat         dog
+#         2        fork       spoon
 #
 # the R code is below:
 tdf <- td.data.frame("test")
-tdf["c7"] <- INITCAP(tdf["c5"])
-as.td.data.frame(tdf, tableName="testZ")
+tdf["c4"] <- INITCAP(tdf["c2"])
+as.td.data.frame(tdf, tableName="test2")
 
-# Below are the results of the table, when you type "select * from testZ"
-#  c1  c2        c3  c4  c5          c6          c7
-#  -----------  --------  --  --  ----------  ----------
-#  5  Emily         ?   cat         dog       Cat
-#  4  Daisy     ?   ?   fork        spoon     Fork
-#  3  Hank      ?   ?   ball        bat       Ball
-#  2  Amy       ?   ?   robot       human     Robot
+# Below are the results of the table, when you type "select * from test2"
+#        c1         c2          c3        c4       
+# ---------- ----------- ---------- ---------
+#         3       ball          bat       Ball
+#         4       robot       human      Robot
+#         1         cat         dog        Cat
+#         2        fork       spoon       Fork
+#
+# To verify each entry of test2
+res1 = tdQuery("select c4 from test2 where c1=3") == "Ball"
+stopifnot(res1)
+
+res2 = tdQuery("select c4 from test2 where c1=4") == "Robot"
+stopifnot(res2)
+
+res3 = tdQuery("select c4 from test2 where c1=1") == "Cat"
+stopifnot(res3)
+
+res4 = tdQuery("select c4 from test2 where c1=2") == "Fork"
+stopifnot(res4)
--- test/testPOWER.R
+++ test/testPOWER.R
@@ -1,21 +1,38 @@
-# Below is the table used for the test, called "tab1"
-# c1           c2           
-# -----------  -----------
-# 5            6
-# 4            8
-# 3            9
-# 1            2
-# 2            7
+# Below is the table used for the test, called "numTab"
+#
+# c1                    c2  c3
+# -----------  -----------  --
+# 5                     2   9
+# 6                     3   9
+# 3                     2   9
+# 8                     3   9
+# 2                     4   9
 
-tdf <- td.data.frame("tab")
-tdf['c3'] <- POWER(tdf['c1'], tdf['c2'])
-as.td.data.frame(tdf, tableName="tab2")
+tdf <- td.data.frame("numTab")
+tdf['c4'] <- POWER(tdf['c1'], tdf['c2'])
+as.td.data.frame(tdf, tableName="numTab2")
 
-#this is the resulting table, called "tab2"
-# c1          c2          c3                                      c
-# ----------- ----------- ----------
-# 5           6           156
-# 4           8           655
-# 3           9           196
-# 1           2
-# 2           7             1
+# this is the resulting table, called "numTab2"
+# c1           c2           c3                                        c4
+# -----------  -----------  --  ----------------------------------------
+# 5            2            9                                         25
+# 6            3            9                                        216
+# 3            2            9                                          9
+# 8            3            9                                        512
+# 2            4            9                                         16
+
+# To verify each entry of c4:
+res1 = tdQuery("select c4 from numTab2 where c1=5") == 25
+stopifnot(res1)
+
+res2 = tdQuery("select c4 from numTab2 where c1=6") == 216
+stopifnot(res2)
+
+res3 = tdQuery("select c4 from numTab2 where c1=3") == 9
+stopifnot(res3)
+
+res4 = tdQuery("select c4 from numTab2 where c1=8") == 512
+stopifnot(res4)
+
+res5 = tdQuery("select c4 from numTab2 where c1=2") == 16
+stopifnot(res5)
